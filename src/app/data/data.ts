c2cee2d186d16339f475eb161af7e80b183c3d0b
renan-augusto <renan_acm1992@hotmail.com>
2023-07-08 16:01:20 -0300

    criacao de componentes

---
+++ src/app/data/data.ts
@@ -0,0 +1,10 @@
+export const data = [
+  {
+    id: '1',
+    title: '1984',
+    description:
+      'Lançado em 1984, o album contém o maior single da banda "Jump" e mais alguns classicos como a música Panama',
+    photoCover:
+      'https://www.ahistoriadodisco.com.br/wp-content/uploads/2018/08/vam_halen_1984.jpg"',
+  },
+];
